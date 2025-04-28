a0b4915c943e98fac4ded66442abc229880b889e
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-04-27 20:26:54 -0700

    Add logic to change grid-box color on hover

--- js/script.js
+++ js/script.js
@@ -4,22 +4,26 @@ const gridContainer = document.querySelector('.grid-container');
 
 let gridWidth = 16;
 let amountOfDivs = gridWidth ** 2;
-const gridArray = [];
 
 for(let i = 0; i < amountOfDivs; i++) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('grid');
     newDiv.style.border = '1px solid grey';
-    newDiv.style.width = findBestSquareSize()
-    newDiv.style.height = findBestSquareSize()
+    newDiv.style.width = findBestSquareSize();
+    newDiv.style.height = findBestSquareSize();
+    newDiv.id = "grid-box"
     
     gridContainer.appendChild(newDiv);
-    gridArray[i] = newDiv;
 }
 
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.id === "grid-box") {
+        e.target.style.backgroundColor = "black";
+    }
+})
 
 function findBestSquareSize() {
-    return ((CONTAINER_SIZE / gridWidth) - 2) + "px";
+    return (CONTAINER_SIZE / gridWidth) - 2 + "px";
 }
 
-console.log(gridArray);
+
