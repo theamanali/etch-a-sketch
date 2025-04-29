d3f03f72b869e50536d54aade7ab48aa4b3b11a3
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-04-29 01:25:57 -0700

    Replace prompt button with slider, invert background

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+// Colors each grid-box could change to when hovered
 const colorStrings = [
+    // 20% opacity to start, will be added onto with later hovers
     "rgba(232,20,22,0.2)",
     "rgba(255,165,0,0.2)",
     "rgba(250,235,54,0.2)",
@@ -9,17 +11,18 @@ const colorStrings = [
 ];
 
 const gridContainer = document.querySelector('.grid-container');
-const resizeButton = document.querySelector(".resize_btn")
 const containerWidth = gridContainer.clientWidth;
-
+const adjustGridSlider = document.querySelector("input");
 let currentGridWidth = 16
+
 createGrid(currentGridWidth);
 
 // Changes color of each grid-box when hovered
 gridContainer.addEventListener("mouseover", (e) => {
     const targetDiv = e.target;
     if (targetDiv.id === "grid-box") {
-        let currentBgColor = window.getComputedStyle(targetDiv).backgroundColor;
+        let currentBgColor = window.getComputedStyle(targetDiv)
+            .backgroundColor;
 
         // check if grid-box is default color
         if (currentBgColor === "rgba(0, 0, 0, 0)") {
@@ -32,42 +35,14 @@ gridContainer.addEventListener("mouseover", (e) => {
     }
 })
 
-resizeButton.addEventListener("click", promptResizing)
-
-function generateRandomColor() {
-    const randomIndex = Math.floor(Math.random() * 7);
-    return colorStrings[randomIndex];
-}
-
-function changeOpacity(element) {
-    const rgbaString = window.getComputedStyle(element).backgroundColor;
-    const [r, g, b, a] = rgbaString.replace(/^rgba?\(|\s+|\)$/g, '').split(",").map(Number)
-    const newA = Math.min(a + 0.2, 1);
-    element.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${newA}`;
-}
-
-
-function promptResizing() {
-    let userGridWidthString = prompt("What grid size would you like? (1 - 100)");
-    let userGridWidth = Number(userGridWidthString);
-    let isNotValidWidth = true;
-
-    // Keep prompting user until valid input
-    while(isNotValidWidth) {
-        if (userGridWidth > 0 && userGridWidth <= 100){
-            isNotValidWidth = false;
-            currentGridWidth = userGridWidth;
-            createGrid(userGridWidth);
-        }
-        else {
-            userGridWidthString = prompt("That is not a valid size! Has to be 1 - 100 (inclusive)");
-            userGridWidth = Number(userGridWidthString)
-        }
-    }
-}
+adjustGridSlider.addEventListener("change", e => {
+    currentGridWidth = e.target.value;
+    createGrid(currentGridWidth);
+})
 
 function createGrid(gridWidth) {
     const hasExistingGrid = gridContainer.hasChildNodes();
+    // If not checked will append to bottom, causing overflow
     if (hasExistingGrid) {
         resetGrid()
     }
@@ -90,10 +65,27 @@ function resetGrid() {
     gridContainer.replaceChildren();
 }
 
-
 function findBestSquareSize() {
     // Find width of each grid-box in pixels
     return (containerWidth / currentGridWidth) + "px";
 }
+
+function generateRandomColor() {
+    const randomIndex = Math.floor(Math.random() * 7);
+    return colorStrings[randomIndex];
+}
+
+function changeOpacity(element) {
+    const rgbaString = window.getComputedStyle(element).backgroundColor;
+    const [r, g, b, a] = rgbaString.replace(
+        /^rgba?\(|\s+|\)$/g, '').split(",").map(Number)
+    const newA = Math.min(a + 0.2, 1);
+    element.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${newA}`;
+}
+
+
+
+
+
 
 
